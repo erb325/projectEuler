d891b7552f00297cd90b58e86b44ff4feb405e97
Ember Baker <emberbaker@Embers-MacBook-Pro.local>
2015-08-03 11:18:12 -0400

    finned problem 7

---
+++ problem7.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+/*By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?*/
+
+var num = 2
+var prime = 0
+var count = 6
+var flag = true
+
+//while count <= 10001 {
+//    if (num % 2 == 0 && num == 2){
+//        //flag = false
+//    }
+//    for ( var i = 3; i <= (num^(1/2)+1); i += 2){
+//        if (num % i == 0 ){
+//            flag =  false
+//        } else {
+//            flag = true
+//        }
+//    }
+//    if flag {
+//        count++
+//        prime = num;
+//    }
+//    
+//    num++
+//    
+//}
+for num = 13; count <= 10001; num += 2{
+    for var i = 3; i < (num / 2); i++ {
+        if num % i == 0 {
+            flag = false
+            break
+        }
+    }
+    if flag {
+        print(num)
+        prime = num
+        count++
+    }
+    flag = true
+}
+
+
+print(prime)
+//104743
